fd7440d562c25b0d7f2f73d6a79921f31e9f8bbc
mhoyos <myshash101@hotmail.com>
2023-03-06 20:07:13 -0500

    Ventana de GLFW vinculada a GLEW, ya se crea una ventana con un fondo rojo y las librerias fueron actualizadas para funcionar en x86 y x64

--- OpenGLProject/OpenGLProject/main.cpp
+++ OpenGLProject/OpenGLProject/main.cpp
@@ -15,6 +15,60 @@ int main()
 		return 1;
 	}
 
+#pragma region Parametros ventana GLFW
+	//Establecer la versión de OpenGL
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	//Habilitar solo código que no este obsoleto y compatible con versiones actualizadas
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	GLFWwindow *pMainWindow = glfwCreateWindow(WIDTH, HEIGHT, "Ventana OpenGL", NULL, NULL);
+
+	if (!pMainWindow)
+	{
+		printf("Fallo al crear la ventana de GLFW");
+		glfwTerminate();
+		return 1;
+	}
+
+	//Buffer de la ventana
+
+	int bufferWidth, bufferHeight;
+	glfwGetFramebufferSize(pMainWindow, &bufferWidth, &bufferHeight);
+
+	//Definir el contexto de GLFW como el que debe usar GLEW
+	glfwMakeContextCurrent(pMainWindow);
+
+	//Permitir extensiones experimentales
+	glewExperimental = GL_TRUE;
+
+	if (glewInit() != GLEW_OK)
+	{
+		printf("Fallo al inicializar GLEW");
+		//Si falla al inicializar GLEW debemos cerrar la ventana de GLFW ya que uno es independiente del otro
+		glfwDestroyWindow(pMainWindow);
+		glfwTerminate();
+		return 1;
+	}
+
+	//OpenGL tamaño viewport, el lugar donde dibujaremos
+	glViewport(0, 0, bufferWidth, bufferHeight);
+#pragma endregion
+
+	//Bucle hasta que la ventana se cierre
+	while (!glfwWindowShouldClose(pMainWindow))
+	{
+		//Aquí podemos manejar el input del usuario
+		glfwPollEvents();
+
+		//Limpiar la ventana
+		glClearColor(1.0f, .0f, .0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(pMainWindow);
+	}
 
 
 	return 0;
